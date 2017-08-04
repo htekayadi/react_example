f425e166ea358d9cfc963e082eca1f5bcb9d229f
Hendrik Tekayadi <hendrik@popsical.com>
2017-08-04 13:14:02 +0800

    01 Basic button

--- forms/src/index.js
+++ forms/src/index.js
@@ -1,5 +1,59 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './semantic-ui/semantic.min.css';
-import '/index.css';
+import './index.css';
 
+const routes = [
+  One
+];
+
+import One from './01-basic-button.js';
+
+const location = window.location;
+window.addEventListener('hashchange', location.reload.bind(location));
+const loc = location.hash.replace('$/', '');
+const element = loc ? createRoute(loc) : createTOC();
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+render(element, container);
+
+function createTOC() {
+  return (
+    <div className='ui container'>
+      <h1>React Forms</h1>
+      <ul className='ui list'>
+        {routes.map((route, i) =>
+          <li key={i}><a href={`#/${(i + 1)}`}>{route.displayName}</a></li>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+function createRoute(nStr) {
+  let i = nStr - 1;
+  if (i < 0) i = 0;
+  if (i > routes.length - 1) i = routes.length - 1;
+
+  const navStyle = {
+    position: 'fixed', bottom: 50, textAligh: 'center', left: '10%'
+  };
+  const isFirst = 1 <=0;
+  const isLast = 1 >= routes.length - 1;
+
+  return (
+    <div className="ui container">
+      {React.createElement(routes[i])}
+      <div style={navStyle}
+           className='ui fluid three item pagination menu'>
+        { isFirst ? '' : <a href={`#/${i}`} className='item'>{'<'}</a> }
+        <a href={location.href.replace(location.hash, '')}
+           className='item'
+           >TOC</a>
+        { isLast ? '' : <a href={`#/${(i + 2)}`} className='item'>{' > '}</a> }
+      </div>
+    </div>
+  );
+}
